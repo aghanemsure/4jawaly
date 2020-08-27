3f4eabbfaa9d5a267d6ba3d03b9ddd2f62ece66f
abdullah ghanem <aghanem@sure.com.sa>
2020-08-27 11:44:06 +0200

    fix log database

--- src/Models/SmsLogModel.php
+++ src/Models/SmsLogModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SmsLogModel extends Model
 {
 
-    protected $table = 'sms_log';
+    protected $table = 'jawaly_sms_log';
     protected $fillable = ['body', 'numbers', 'status', 'gate_message', 'response', 'sender'];
 
 }
